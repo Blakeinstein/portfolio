3a006e3659eba9b969df2c786ab9646de2084519
Blakeinstein <rishi1998@gmail.com>
2023-06-19 17:21:28 -0700

    chore: cleanup

--- src/lib/components/buildings/elements/buildings.svelte
+++ src/lib/components/buildings/elements/buildings.svelte
@@ -1,32 +1,6 @@
 <script lang="ts">
 	import { GLTF } from '@threlte/extras'
-	import { T, useFrame, useLoader } from '@threlte/core';
-	import { Text } from '@threlte/extras';
-	import { makeCityScape } from '../utils';
-	import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-	import { MeshPhysicalMaterial } from 'three';
-	import { CITYSCAPE_CONFIG } from '$lib/config/scene';
-	import { offsetSize } from '../context';
-
-	// let posA: [number, number, number] = [0, 1, 0];
-	// let posB: [number, number, number] = [0, 1, $offsetSize];
-
-	// const material = new MeshPhysicalMaterial(CITYSCAPE_CONFIG.material);
-
-	// const objLoader = useLoader(OBJLoader);
-
-	// const models = objLoader.load('/models/buildings.obj');
-
-	// const groups = models.then((buildings) => {
-	// 	const group = makeCityScape(material, buildings.children);
-	// 	return [group, group.clone()];
-	// });
-
-	// useFrame(({ clock }) => {
-	// 	const delta = clock.getElapsedTime();
-	// 	posA[2] = (delta * 5) % $offsetSize;
-	// 	posB[2] = posA[2] - $offsetSize;
-	// });
+	import { useFrame } from '@threlte/core';
 
 	let rotation = 0;
 	useFrame(({ clock }) => {
@@ -35,12 +9,6 @@
 	});
 </script>
 
-<!-- {#await groups then [groupA, groupB]}
-	<T is={groupA} bind:position={posA} />
-	<T is={groupB} bind:position={posB} />
-{:catch error}
-	<Text text={error.message} fontSize={0.01} />
-{/await} -->
 <GLTF
 	castShadow
 	receiveShadow
