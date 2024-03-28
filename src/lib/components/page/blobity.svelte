929156911af2a3d77028f9230bad50e9f1cbd403
Blakeinstein <rishi1998@gmail.com>
2024-03-27 23:09:21 -0700

    bump deps

--- src/lib/components/page/blobity.svelte
+++ src/lib/components/page/blobity.svelte
@@ -22,13 +22,3 @@
 		blob['canvas'].style.zIndex = 0;
 	}
 </script>
-
-<style>
-	:global(html>canvas) {
-		view-transition-name: blobity;
-	}
-
-	::view-transition-group(blobity) {
-		animation: none;
-	}
-</style>
